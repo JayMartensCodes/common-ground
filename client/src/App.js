8376e45eb28d22f8358c5648f0ba26f4d9333d33
Declan O'Donnell <dexyod43@gmail.com>
2020-09-25 13:49:21 -0400

    set-up inital map with basic search

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,70 @@
 import React from 'react';
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import LocateHome from './components/LocateHome'
+import Search from './components/Search'
+import mapStyles from "./mapStyles";
 import './App.css';
 
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  height: "100vh",
+  width: "100vw",
+};
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+const center = {
+  lat: 43.6532,
+  lng: -79.3832,
+};
+
+
 function App() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, []);
+
+  if (loadError) return "Error";
+  if (!isLoaded) return "Loading...";
+
   return (
-    <div className="App">
-      COMMON GROOOOOOOOOOOUND
+    <div>
+      <h1>
+        Common Ground!
+      </h1>
+
+      <LocateHome panTo={panTo} />
+      <Search panTo={panTo} />
+
+      <GoogleMap
+        id="map"
+        mapContainerStyle={mapContainerStyle}
+        zoom={12}
+        center={center}
+        options={options}
+        // onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+      </GoogleMap>
     </div>
   );
 }
---
+++ client/src/components/LocateHome.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './LocateHome.css';
+import logo from '../images/rare-items-rube-potato.png';
+
+
+function LocateHome({ panTo }) {
+  return (
+    <button
+      className="locate"
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            panTo({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            });
+          },
+          () => null
+        );
+      }}
+    >
+      <img src={logo} alt="potato" />
+    </button>
+  );
+}
+
+export default LocateHome;
