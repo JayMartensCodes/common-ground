d6a185987202cdeb4e2a0bd84fa2784ff45ac86c
Declan O'Donnell <dexyod43@gmail.com>
2020-09-29 14:16:42 -0400

    got filter changed to be under search bar. Now renders within search so we can use state

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -17,7 +17,6 @@ function NavBar({ panTo, currentLocation }) {
         </Nav>
         <Nav>
           <Search currentLocation={currentLocation} panTo={panTo} />
-          <Filter />
           <LocateHome panTo={panTo} />
         </Nav>
       </Navbar.Collapse>
