c1b5bc3baffde6636e5fe89a48587167a6cdf1cb
juliannalim <julianna.j.lim@gmail.com>
2020-09-30 13:07:49 -0400

    drop down works now and changed the link names

--- client/src/components/Wannabedropdown.js
+++ client/src/components/Wannabedropdown.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import './LocateHome.css';
+import Button from "react-bootstrap/Button";
+import Dropdown from 'react-bootstrap/Dropdown';
 // import logo from '../images/rare-items-rube-potato.png';
 
 
-function DropDown({ panTo }) {
+function DropDown() {
   return (
-    <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button"
-        id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Dropdown button
-      </button>
-      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a class="dropdown-item" href="#">Messages</a>
-        <a class="dropdown-item" href="#">Friends</a>
-        <a class="dropdown-item" href="#">Share Map</a>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="#">Logout</a>
-      </div>
-    </div>
+    <Dropdown>
+      <Dropdown.Toggle variant="success" size="lg" id="dropdown-basic">
+        Dropdown Button
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Message</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Find Friends</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Share Location</Dropdown.Item>
+        <Dropdown.Divider />
+        <Dropdown.Item eventKey="4">Log Out</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
   );
 }
 
