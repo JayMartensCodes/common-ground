67a57f020fa83fb5f5ef457b9b487e86f23481d7
Jay Martens <martensjay93@gmail.com>
2020-09-30 14:36:50 -0400

    Removed home_address from the users table

--- server/db/schema/01_users.sql
+++ server/db/schema/01_users.sql
@@ -5,6 +5,5 @@ CREATE TABLE users (
   last_name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  home_address VARCHAR(255) NOT NULL,
   UNIQUE(email)
 );
