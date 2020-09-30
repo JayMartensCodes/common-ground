cbe4df6149b7b394537594f957e64e87f161ecaa
Declan O'Donnell <dexyod43@gmail.com>
2020-09-30 17:40:23 -0400

    navbar work

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,32 +1,33 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Button from "react-bootstrap/Button";
-import Nav from "react-bootstrap/Nav";
+
 import Search from "./Search";
+
 import LocateHome from "./LocateHome";
 import Filter from "./Filter";
+import { Navbar, Nav } from "react-bootstrap";
 import LoginModal from "./LoginModal";
 import SignupModal from "./SignupModal";
 import DropDown from "./Wannabedropdown";
+import './Navbar.css';
 
 function NavBar({
   panTo,
   currentLocation,
   setDestination,
   setFilterOption,
-  setMidpoint
+  setMidpoint,
 }) {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar className="navbar" collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Navbar.Brand href="#home">Common-Ground</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav>
           <LoginModal />
           <SignupModal />
         </Nav>
         <Nav>
-        <Filter setFilterOption={setFilterOption} />
+          {/* <Filter setFilterOption={setFilterOption} /> */}
           <Search
             currentLocation={currentLocation}
             panTo={panTo}
@@ -34,7 +35,6 @@ function NavBar({
             setFilterOption={setFilterOption}
             setMidpoint={setMidpoint}
           />
-          <LocateHome panTo={panTo} />
         </Nav>
         <Nav>
           <DropDown />
