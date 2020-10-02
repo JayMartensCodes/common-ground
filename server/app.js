ac0b405a154cf8652ea6dcb9e6032c39a0f2eb6f
juliannalim <julianna.j.lim@gmail.com>
2020-10-02 13:51:36 -0400

    socket io is now connected

--- server/app.js
+++ server/app.js
@@ -1,18 +1,21 @@
 const createError = require('http-errors');
 const express = require('express');
 const bodyParser = require('body-parser')
-const http = require("http");
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const socketIo = require('socket.io');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
 const db = require('./db');
 const dbHelpers = require('./helpers/dbHelpers')(db);
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const socketIo = require('socket.io');
+const io = socketIo(server);
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -28,23 +31,23 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter(dbHelpers));
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
-module.exports = app;
+server.listen(3001);
