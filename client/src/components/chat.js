a1c20c687a88ebdb7f96ed1cabc9aae6fdbcfd07
juliannalim <julianna.j.lim@gmail.com>
2020-10-02 18:05:17 -0400

    chat formatting

--- client/src/components/chat.js
+++ client/src/components/chat.js
@@ -1,23 +1,19 @@
-import './Chat.css';
-// Make connection
-var socket = io.connect('http://localhost:4000');
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import useSocket from 'use-socket.io-client';
+const io = require('socket.io')(3000);
 
-// Query DOM
-var message = document.getElementById('message'),
-  handle = document.getElementById('handle'),
-  btn = document.getElementById('send'),
-  output = document.getElementById('output');
 
-// Emit events
-btn.addEventListener('click', function () {
-  socket.emit('chat', {
-    message: message.value,
-    handle: handle.value
-  });
-  message.value = "";
-});
+function Chat() {
+  return (
 
-// Listen for events
-socket.on('chat', function (data) {
-  output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
-});
+    <div style={{ textAlign: 'center', margin: '30vh auto', width: '70%' }}>
+      <Form onSubmit={event => handleSubmit(event)}>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </div>
+  );
+}
+
+export default Chat;
